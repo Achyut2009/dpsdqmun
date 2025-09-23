5fbc28f39a3b7268d0522da38ad8c7e2a89288a8
Achyut Paliwal <achyutkpaliwal@gmail.com>
2025-09-23 15:43:51 +0300

    13th commit

--- src/components/contact-modal.tsx
+++ src/components/contact-modal.tsx
@@ -19,7 +19,7 @@ export default function ContactModal() {
                     <DialogDescription className="text-md">
                         Amir Khan Sir: +974 3392 2764 <br />
                         Kakoli Biswas Ma&apos;am: +974 ---- ---- <br />       
-                        Telma D'Souza Ma&apos;am: +974 ---- ---- <br />
+                        Telma D&apos;Souza Ma&apos;am: +974 ---- ---- <br />
                     </DialogDescription>
                 </DialogHeader>
             </DialogContent>
